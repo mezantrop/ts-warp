34e54af8349820ba6078c973ec6e927af5a8a01c
Mikhail Zakharov <zmey20000@yahoo.com>
2022-02-07 15:14:35 +0100

    LICENSE, examples update

--- examples/ts-warp_iptables.sh
+++ examples/ts-warp_iptables.sh
@@ -33,12 +33,12 @@
 iptables -t nat -N SOCKS
 
 # Exclude SOCKS servers
-iptables -t nat -A SOCKS -d 9.45.121.62,192.168.1.237,129.39.133.102 -j RETURN
+iptables -t nat -A SOCKS -d 10.0.12.1,192.168.1.237,123.45.1.11 -j RETURN
 
 # Destination network/address definitions
-iptables -t nat -A SOCKS -p tcp -d 10.101.0.0/16 -j REDIRECT --to-ports 10800
+iptables -t nat -A SOCKS -p tcp -d 10.0.10.0/16 -j REDIRECT --to-ports 10800
 iptables -t nat -A SOCKS -p tcp -d 192.168.1.0/24,192.168.3.0/24 -j REDIRECT --to-ports 10800
-iptables -t nat -A SOCKS -p tcp -d 129.39.133.0/24,129.39.143.0/24 -j REDIRECT --to-ports 10800
+iptables -t nat -A SOCKS -p tcp -d 123.45.123.0/24,123.45.234.96/24 -j REDIRECT --to-ports 10800
 
 # NB! User 'frodo' is a process owner username of the applictions to redirect to
 # SOCKS servers. Change it to your username on localhost.
