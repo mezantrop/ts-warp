1355ad25e2682db8a88e6d178d14ec989bc32014
Mikhail Zakharov <zmey20000@yahoo.com>
2022-09-19 17:41:22 +0200

    socks5_atype(): Address type selector: IPv4/IPv6/DNS

--- inifile.h
+++ inifile.h
@@ -104,3 +104,4 @@ struct ini_section *ini_look_server(struct ini_section *ini, struct sockaddr ip)
 int create_chains(struct ini_section *ini, struct chain_list *chain);
 struct ini_section *getsection(struct ini_section *ini, char *name);
 int chk_inivar(void *v, char *vi, int d);
+int socks5_atype(ini_section *ini, struct sockaddr daddr);
