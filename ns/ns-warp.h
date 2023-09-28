8f07b60044b29dd929b1218341a794c51800a789
Mikhail Zakharov <zmey20000@yahoo.com>
2023-09-28 12:57:58 +0200

    `NS-Warp` roll-back to stable

--- ns/ns-warp.h
+++ ns/ns-warp.h
@@ -40,14 +40,12 @@
 #define NS_LISTEN_PORT  "5353"                                          /* This is our UDP port */
 #define DNS_PORT        "53"                                            /* That is remote DNS server port */
 
-#define STR_SIZE        255
-
 #define RUNAS_USER      "nobody"
 
 /* Program name and version */
 #define NS_PROG_NAME          "NS-Warp"
 #define NS_PROG_NAME_SHORT    "NSW"
-#define NS_PROG_VERSION       "1.0.5"
+#define NS_PROG_VERSION       "1.0.6"
 
 /* -- Function prototypes ------------------------------------------------------------------------------------------- */
 void trap_signal(int sig);
