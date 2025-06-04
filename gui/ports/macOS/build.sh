69df0083053631ff80549bceab4e04a99ff0972d
Mikhail Zakharov <zmey20000@yahoo.com>
2025-06-04 19:35:19 +0200

    `libssl/libssh` section tweaks

--- gui/ports/macOS/build.sh
+++ gui/ports/macOS/build.sh
@@ -13,58 +13,57 @@ rm -rf build dist venv tmp GUI-Warp *.dmg
 echo "-- Making binaries ----------------------------------------------------------------------------------------------" &&
 # NB! Static libraries must be located in the current directory! If changing, adjust Makefile as well!
 [ -n "$WITH_LIBSSH2" ] && {
-	echo "-- Downloading/unpacking SSL and SSH2 libraries -----------------------------------------------------------------" &&
-	mkdir tmp &&
-	wget -O tmp/head-openssl.tgz https://github.com/openssl/openssl/archive/refs/heads/master.tar.gz &&
-	wget -O tmp/head-libssh2.tgz https://github.com/libssh2/libssh2/archive/refs/heads/master.tar.gz &&
-	cd tmp &&
-	tar zxvf head-openssl.tgz &&
-	tar zxvf head-libssh2.tgz &&
+	echo "-- Downloading/unpacking SSL and SSH2 libraries -----------------------------------------------------------------"
+	mkdir tmp
+	wget -O tmp/head-openssl.tgz https://github.com/openssl/openssl/archive/refs/heads/master.tar.gz
+	wget -O tmp/head-libssh2.tgz https://github.com/libssh2/libssh2/archive/refs/heads/master.tar.gz
+	cd tmp
+	tar zxvf head-openssl.tgz
+	tar zxvf head-libssh2.tgz
 
-	echo "-- Compiling SSL for ARM64 processors ---------------------------------------------------------------------------" &&
-	cd openssl-master &&
+	echo "-- Compiling SSL for ARM64 processors ---------------------------------------------------------------------------"
+	cd openssl-master
 
-	./Configure darwin64-arm64-cc no-shared -mmacosx-version-min=11 -no-tests &&
-	make depend &&
-	make -j 4 build_libs &&
-	cp libcrypto.a ../arm64_libcrypto.a &&
-	cp libssl.a  ../arm64_libssl.a &&
+	./Configure darwin64-arm64-cc no-shared -mmacosx-version-min=11 -no-tests
+	make depend
+	make -j 4 build_libs
+	cp libcrypto.a ../arm64_libcrypto.a
+	cp libssl.a  ../arm64_libssl.a
 
+	echo "-- Compiling SSL for x86_64 processors --------------------------------------------------------------------------"
+	make clean
+	./Configure darwin64-x86_64-cc no-shared -mmacosx-version-min=11 -no-tests
+	make depend
+	make -j 4 build_libs
+	cp libcrypto.a ../x86_64_libcrypto.a
+	cp libssl.a  ../x86_64_libssl.a
 
-	echo "-- Compiling SSL for x86_64 processors --------------------------------------------------------------------------" &&
-	make clean &&
-	./Configure darwin64-x86_64-cc no-shared -mmacosx-version-min=11 -no-tests &&
-	make depend &&
-	make -j 4 build_libs &&
-	cp libcrypto.a ../x86_64_libcrypto.a &&
-	cp libssl.a  ../x86_64_libssl.a &&
+	echo "-- Compiling SSH2 for ARM64 processors --------------------------------------------------------------------------"
+	cd ../libssh2-master
+	export CFLAGS="-arch arm64 -pipe -no-cpp-precomp  -mmacosx-version-min=11"
+	export CPPFLAGS="-arch arm64 -pipe -no-cpp-precomp -mmacosx-version-min=11"
+	autoreconf -fi
+	./configure --host=aarch64-apple-darwin --disable-debug --disable-dependency-tracking --disable-silent-rules --disable-examples-build --with-libz --disable-shared --enable-static
+	make -j
+	cp src/.libs/libssh2.a ../arm64_libssh2.a
 
-	echo "-- Compiling SSH2 for ARM64 processors --------------------------------------------------------------------------" &&
-	cd ../libssh2-master &&
-	export CFLAGS="-arch arm64 -pipe -no-cpp-precomp  -mmacosx-version-min=11" &&
-	export CPPFLAGS="-arch arm64 -pipe -no-cpp-precomp -mmacosx-version-min=11" &&
-	autoreconf -fi &&
-	./configure --host=aarch64-apple-darwin --disable-debug --disable-dependency-tracking --disable-silent-rules --disable-examples-build --with-libz --disable-shared --enable-static &&
-	make -j 4 &&
-	cp src/.libs/libssh2.a ../arm64_libssh2.a &&
+	make clean
+	export CFLAGS="-arch x86_64 -pipe -no-cpp-precomp -mmacosx-version-min=11"
+	export CPPFLAGS="-arch x86_64 -pipe -no-cpp-precomp -mmacosx-version-min=11"
+	autoreconf -fi
+	./configure --host=x86_64-apple-darwin --disable-debug --disable-dependency-tracking --disable-silent-rules --disable-examples-build --with-libz --disable-shared --enable-static
+	make -j 4
+	cp src/.libs/libssh2.a ../x86_64_libssh2.a
 
-	make clean &&
-	export CFLAGS="-arch x86_64 -pipe -no-cpp-precomp -mmacosx-version-min=11" &&
-	export CPPFLAGS="-arch x86_64 -pipe -no-cpp-precomp -mmacosx-version-min=11" &&
-	autoreconf -fi &&
-	./configure --host=x86_64-apple-darwin --disable-debug --disable-dependency-tracking --disable-silent-rules --disable-examples-build --with-libz --disable-shared --enable-static &&
-	make -j 4 &&
-	cp src/.libs/libssh2.a ../x86_64_libssh2.a &&
+	unset CFLAGS
+	unset CPPFLAGS
 
-	unset CFLAGS &&
-	unset CPPFLAGS &&
-
-	echo "-- Making universal libraries -----------------------------------------------------------------------------------" &&
-	cd .. &&
-	lipo -create arm64_libssl.a x86_64_libssl.a -output ../libssl.a &&
-	lipo -create arm64_libcrypto.a x86_64_libcrypto.a -output ../libcrypto.a &&
-	lipo -create arm64_libssh2.a x86_64_libssh2.a -output ../libssh2.a &&
-	cd .. &&
+	echo "-- Making universal libraries -----------------------------------------------------------------------------------"
+	cd ..
+	lipo -create arm64_libssl.a x86_64_libssl.a -output ../libssl.a
+	lipo -create arm64_libcrypto.a x86_64_libcrypto.a -output ../libcrypto.a
+	lipo -create arm64_libssh2.a x86_64_libssh2.a -output ../libssh2.a
+	cd ..
 
 	[ ! -f ./libssh2.a -o ! -f ./libcrypto.a -o ! -f libssl.a ] && {
 		echo "Unable to find static SSH2 library, skipping it"
