22be5265cf4a2d583ed8119d05c34e1b6394c40d
Mikhail Zakharov <zmey20000@yahoo.com>
2026-02-08 17:33:46 +0100

    patch `iSSH2-head.sh`

--- gui/ports/macOS/iSSH2/iSSH2-head.sh
+++ gui/ports/macOS/iSSH2/iSSH2-head.sh
@@ -287,11 +287,11 @@ echo
 set -e
 
 if [[ $BUILD_SSL == true ]]; then
-  "$BASEPATH/iSSH2-openssl.sh" || cleanupFail $CLEAN_BUILD
+  sh "$BASEPATH/iSSH2-openssl.sh" || cleanupFail $CLEAN_BUILD
 fi
 
 if [[ $BUILD_SSH == true ]]; then
-  "$BASEPATH/iSSH2-libssh2.sh" || cleanupFail $CLEAN_BUILD
+  sh "$BASEPATH/iSSH2-libssh2.sh" || cleanupFail $CLEAN_BUILD
 fi
 
 if [[ $BUILD_SSL == true ]] || [[ $BUILD_SSH == true ]]; then
